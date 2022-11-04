68f134a9c4026afde328a55997eef2185c296a05
user.email <0kq@0kqnet.work>
2022-11-04 23:08:06 +0900

    update voiceroid.py

--- bot_modules/voiceroid.py
+++ bot_modules/voiceroid.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 class voiceroid:
   def __init__(self,address):
@@ -6,9 +7,13 @@ class voiceroid:
 
   def generate(self,text,path,actor,mode,speed,pitch):
     with requests.Session() as session:
-      req = f"{self.address}generate?text={text}&speaker={actor}&mode={mode}&speed={speed}&pitch={pitch}"
-      #req = urllib.parse.urlencode(req,encoding="shift-jis")
-      s = session.get(req)
-      with open(path,mode="wb") as f:
-        f.write(s.content)
+      for i in range(5):
+        req = f"{self.address}generate?text={text}&speaker={actor}&mode={mode}&speed={speed}&pitch={pitch}"
+        #req = urllib.parse.urlencode(req,encoding="shift-jis")
+        s = session.get(req)
+        with open(path,mode="wb") as f:
+          f.write(s.content)
+        if s.status_code == 200:
+          break
+        time.sleep(1)
     #sound_controller.convert_volume(path+".wav",0.7)
